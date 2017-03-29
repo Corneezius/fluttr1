84537280fb5b88fe75a7242999fc9752850421c1
Corneezius <cfr2116@columbia.edu>
2017-03-29 04:37:32 -0400

    add function that scrolls to top of page on reload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+
+// Scroll to top of page on reload
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
+// Fade in header as page scrolls down
 $(window).scroll(function() {
   var scrollTop = $(this).scrollTop();
 
@@ -20,7 +27,7 @@ $(window).scroll(function() {
   });
 });
 
-
+// Change background color of div each tim DOM loads
 $(document).ready(function() {
     var background = [ 'cover-bg-one', 'cover-bg-two', 'cover-bg-three', 'cover-bg-five', 'cover-bg-six', 'cover-bg-seven', 'cover-bg-eight', 'cover-bg-nine', 'cover-bg-ten', 'cover-bg-eleven', 'cover-bg-twelve', 'cover-bg-thirteen' ];
     $('#cover').each(function(i, val) {
